2b8fd2e6df7ce43af92f9ee6bd5a690e0b62c619
quicklikerabbit <steven.d.richards@gmail.com>
2017-11-05 21:31:55 -0800

    remove console logs

--- index.js
+++ index.js
@@ -3,35 +3,31 @@ const config = require("./config.json")
 
 exports.helloGCS = function(event, callback) {
   const file = event.data;
-  console.log(file);
 
   if (file.resourceState === "not_exists") {
-    console.log(`File ${file.name} deleted.`);
     axios({
       method: 'post',
       url: config.SLACK_WEBHOOK,
       data: {
-        text: `File: ${file.name} was deleted!`
+        text: `File: ${file.name} was deleted from ${file.bucket}`
       }
     });
   } else if (file.metageneration === "1") {
     // metageneration attribute is updated on metadata changes.
     // on create value is 1
-    console.log(`File ${file.name} uploaded.`);
     axios({
       method: 'post',
       url: config.SLACK_WEBHOOK,
       data: {
-        text: `File: ${file.name} was uploaded!`
+        text: `File: ${file.name} was uploaded to ${file.bucket}.`
       }
     });
   } else {
-    console.log(`File ${file.name} metadata updated.`);
     axios({
       method: 'post',
       url: config.SLACK_WEBHOOK,
       data: {
-        text: `File: ${file.name} metadata updated!`
+        text: `File: ${file.name} metadata updated.`
       }
     });
   }
