c85d0e03d5e7b330e3dba2b87300a55dc34a095d
quicklikerabbit <steven.d.richards@gmail.com>
2017-11-05 17:25:56 -0800

    add webhook calls

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
-exports.helloGCS = function (event, callback) {
-  const file = event.data;
+const axios = require("axios");
+const config = require("./config.json")
 
-  if (file.resourceState === 'not_exists') {
+exports.helloGCS = function(event, callback) {
+  const file = event.data;
+  console.log(file);
+
+  if (file.resourceState === "not_exists") {
     console.log(`File ${file.name} deleted.`);
-  } else if (file.metageneration === '1') {
+    axios({
+      method: 'post',
+      url: config.SLACK_WEBHOOK,
+      data: {
+        text: `File: ${file.name} was deleted!`
+      }
+    });
+  } else if (file.metageneration === "1") {
     // metageneration attribute is updated on metadata changes.
     // on create value is 1
     console.log(`File ${file.name} uploaded.`);
+    axios({
+      method: 'post',
+      url: config.SLACK_WEBHOOK,
+      data: {
+        text: `File: ${file.name} was uploaded!`
+      }
+    });
   } else {
     console.log(`File ${file.name} metadata updated.`);
+    axios({
+      method: 'post',
+      url: config.SLACK_WEBHOOK,
+      data: {
+        text: `File: ${file.name} metadata updated!`
+      }
+    });
   }
 
   callback();
